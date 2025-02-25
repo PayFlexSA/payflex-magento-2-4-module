f46279ca85f120996ed843adee9ce5c156ca8ba5
Nicholas Hoffmann <nmjbhoffmann@gmail.com>
2025-02-25 18:28:41 +0200

    Added extra checks to Cron info

--- Helper/Support.php
+++ Helper/Support.php
@@ -17,6 +17,8 @@ class Support extends AbstractHelper
 
     private $objectManager;
 
+    private $timezone;
+
 
 
     public function __construct(
@@ -27,7 +29,14 @@ class Support extends AbstractHelper
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $this->objectManager = $objectManager;
         $context = $objectManager->get('Magento\Framework\App\Helper\Context');
-        
+
+        // Get system timezone
+        $timezone = $objectManager->create('Magento\Framework\Stdlib\DateTime\TimezoneInterface');
+        $timezone->getConfigTimezone();
+        $timezone->date();
+
+        $timezone = new \DateTimeZone($timezone->getConfigTimezone());
+        $this->timezone = $timezone;
 
         $date = $objectManager->get('Magento\Framework\Stdlib\DateTime\DateTime');
         $this->communication = new Communication($context, $date);
@@ -165,13 +174,13 @@ class Support extends AbstractHelper
             $timezone = $this->objectManager->create('Magento\Framework\Stdlib\DateTime\TimezoneInterface');
             $timezone->getConfigTimezone();
             $timezone->date();
-            $now            = new \DateTime('now', new \DateTimeZone($timezone->getConfigTimezone()));
+            $now            = new \DateTime('now', $this->timezone);
             $checkTime      = new \DateTime($cron_check_time);
             $interval       = $now->diff($checkTime);
             $cron_till_time = '';
             
             if ($checkTime > $now) {
-                $cron_till_time = "Cron will start checking in " . $interval->format('%i minutes');
+                $cron_till_time = "Cron will start checking in " . $interval->format('%i minute(s)');
             } else {
                 $cron_till_time = "Currently in queue";
             }
@@ -182,8 +191,105 @@ class Support extends AbstractHelper
             $order_link = '';
             $order_link = $this->objectManager->get('Magento\Store\Model\StoreManagerInterface')->getStore($value['store_id'])->getBaseUrl() . "admin/sales/order/view/order_id/" . $value['entity_id'];
             $orders[$id]['order_link'] = $order_link;
-
         }
         return $orders;
+    }
+
+    /**
+     * Get the last time the Magento cron was run, in human readable. This is the last time the main magento cron was run, not the payflex cron
+     */
+    public function getLastCronRanTime()
+    {
+        $cron_schedule = $this->objectManager->create('Magento\Cron\Model\ResourceModel\Schedule\Collection');
+        $cron_schedule->addFieldToFilter('job_code', 'payflex_gateway_check_order_status');
+        $cron_schedule->addFieldToFilter('status', 'success');
+        $cron_schedule->setOrder('executed_at', 'desc');
+        $cron_schedule->setPageSize(1);
+        $cron_schedule->setCurPage(1);
+        $cron_schedule->load();
+        $last_cron_time = '';
+        foreach($cron_schedule as $cron)
+        {
+
+            $last_cron_time = $cron->getExecutedAt();
+        }
+
+        if($last_cron_time == '')
+        {
+            return "Cron has not been run yet";
+        }
+
+        // Get current system timezone
+        $timezone = $this->objectManager->create('Magento\Framework\Stdlib\DateTime\TimezoneInterface');
+        $timezone->getConfigTimezone();
+        $timezone->date();
+        $now            = new \DateTime('now');
+        $checkTime      = new \DateTime($last_cron_time);
+        
+        // use get_date_diff
+        $cron_till_time = $this->get_date_diff($checkTime->format('Y-m-d H:i:s'), $now->format('Y-m-d H:i:s')). " ago";
+
+        return $cron_till_time;
+    }
+
+    /**
+     * Get the difference between 2 dates in human readable format
+     */
+    private function get_date_diff( $time1, $time2, $precision = 2 ) {
+        // If not numeric then convert timestamps
+        if( !is_int( $time1 ) ) {
+            $time1 = strtotime( $time1 );
+        }
+        if( !is_int( $time2 ) ) {
+            $time2 = strtotime( $time2 );
+        }
+    
+        // If time1 > time2 then swap the 2 values
+        if( $time1 > $time2 ) {
+            list( $time1, $time2 ) = array( $time2, $time1 );
+        }
+    
+        // Set up intervals and diffs arrays
+        $intervals = array( 'year', 'month', 'day', 'hour', 'minute', 'second' );
+        $diffs = array();
+    
+        foreach( $intervals as $interval ) {
+            // Create temp time from time1 and interval
+            $ttime = strtotime( '+1 ' . $interval, $time1 );
+            // Set initial values
+            $add = 1;
+            $looped = 0;
+            // Loop until temp time is smaller than time2
+            while ( $time2 >= $ttime ) {
+                // Create new temp time from time1 and interval
+                $add++;
+                $ttime = strtotime( "+" . $add . " " . $interval, $time1 );
+                $looped++;
+            }
+    
+            $time1 = strtotime( "+" . $looped . " " . $interval, $time1 );
+            $diffs[ $interval ] = $looped;
+        }
+    
+        $count = 0;
+        $times = array();
+        foreach( $diffs as $interval => $value ) {
+            // Break if we have needed precission
+            if( $count >= $precision ) {
+                break;
+            }
+            // Add value and interval if value is bigger than 0
+            if( $value > 0 ) {
+                if( $value != 1 ){
+                    $interval .= "s";
+                }
+                // Add value and interval to times array
+                $times[] = $value . " " . $interval;
+                $count++;
+            }
+        }
+    
+        // Return string with times
+        return implode( ", ", $times );
     }
 }
